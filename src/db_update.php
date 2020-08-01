120409bc6979f0ed562def515dc9c74d13f4ed26
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-08-01 23:06:59 +0200

    db_update: considers date column for guid generation

--- src/db_update.php
+++ src/db_update.php
@@ -36,29 +36,44 @@ try {
 echo 'Updating GUIDs.' . PHP_EOL;
 
 $guid_tables = [
-    'divisions',
-    'log_credits',
-    'log_diamonds',
-    'log_diamonds_ads',
-    'log_drops',
-    'song_clear_cards',
-    'song_clears_v2',
-    'songs',
-    'themes',
-    'user_credentials',
-    'users',
+    'divisions' => [ 'date_column' => false ],
+    'league_ranking' => [ 'date_column' => 'date' ],
+    'log_credits' => [ 'date_column' => 'date' ],
+    'log_diamonds' => [ 'date_column' => 'date' ],
+    'log_diamonds_ads' => [ 'date_column' => 'date' ],
+    'log_drops' => [ 'date_column' => 'date' ],
+    'song_clear_cards' => [ 'date_column' => false ],
+    'song_clears_v2' => [ 'date_column' => 'date' ],
+    'songs' => [ 'date_column' => false ],
+    'superstar_games' => [ 'date_column' => false ],
+    'themes' => [ 'date_column' => false ],
+    'user_credentials' => [ 'date_column' => 'created' ],
+    'users' => [ 'date_column' => 'created' ],
 ];
 
-foreach ($guid_tables as $table_name) {
+foreach ($guid_tables as $table_name => $table_options) {
     $count = $pdo->fetchCol("SELECT COUNT(*) FROM `$table_name` WHERE guid IS NULL")[0];
     if (!$count) {
         echo sprintf('Skipping table "%s" - no entries to update.', $table_name) . PHP_EOL;
         continue;
     }
     echo sprintf('Table "%s" [%dx]: ', $table_name, $count);
-    foreach ($pdo->yieldCol("SELECT id FROM `$table_name` WHERE guid IS NULL") as $row_id) {
-        $pdo->perform("UPDATE `$table_name` SET guid = :guid WHERE id = :id", [ 'guid' => guid_generate($dbconfig['host'], $dbconfig['name']), 'id' => $row_id ]);
-        echo '.';
+    if ($table_options['date_column']) {
+        foreach ($pdo->yieldPairs("SELECT id, `${table_options['date_column']}` FROM `$table_name` WHERE guid IS NULL") as $row_id => $date) {
+            $pdo->perform(
+                "UPDATE `$table_name` SET guid = :guid WHERE id = :id",
+                [ 'guid' => guid_generate($dbconfig['host'], $dbconfig['name'], strtotime($date)), 'id' => $row_id ]
+            );
+            echo '.';
+        }
+    } else {
+        foreach ($pdo->yieldCol("SELECT id FROM `$table_name` WHERE guid IS NULL") as $row_id) {
+            $pdo->perform(
+                "UPDATE `$table_name` SET guid = :guid WHERE id = :id",
+                [ 'guid' => guid_generate($dbconfig['host'], $dbconfig['name']), 'id' => $row_id ]
+            );
+            echo '.';
+        }
     }
     echo PHP_EOL;
 }
