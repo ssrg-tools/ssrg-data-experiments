ccab5a3f08eb7b74b5de322c468585db6976e7bc
Anh Nhân Nguyễn <anhnhan@outlook.com>
2020-08-05 20:00:37 +0200

    iterate beatmap drawing, clean up code

--- src/beatmaps_draw.php
+++ src/beatmaps_draw.php
@@ -15,19 +15,23 @@ natsort($beatmap_files);
 
 $imagine = new Imagine\Imagick\Imagine();
 
-$size_row = 15;
-$size_beat = 10;
-$size_sub_beat = 5;
+$scale_overall = 1.0;
+$scale_vertical = 1.0 * $scale_overall;
+$scale_horizontal = 1.0 * $scale_overall;
+$scale_subbeat = 1.0 * $scale_overall;
 
-$scale_vertical = 1.2;
-$scale_horizontal = 1.0;
-$scale_subbeat = 1;
+$size_row = 40 * $scale_overall;
+$size_beat = 15 * $scale_overall;
+$size_sub_beat = 15 * $scale_overall;
+$size_slider_line = 6 * $scale_overall;
 
 $color_tap = '59e';
 $color_slider = 'ed8';
+$color_slider_slide = 'dc7';
 
-$image_width = 150;
-$margin_side = 25;
+$image_width = 200 * $scale_horizontal;
+$margin_side = 25 * $scale_horizontal;
+$margin_top = 50 * $scale_vertical;
 $beat_x_start = $margin_side;
 $beat_x_end   = $image_width - $margin_side;
 $beat_x_width = $beat_x_end - $beat_x_start;
@@ -42,61 +46,70 @@ foreach ($beatmap_files as $beatmap_file) {
     $songdata = json_decode(file_get_contents($path_beatmaps . DS . $beatmap_file), true);
     $beatmap = $songdata['map'];
 
-    echo $songdata['song_filename'] . ' [' . $songdata['difficulty'] . ']' . PHP_EOL;
+    echo $songdata['dalcom_beatmap_filename'] . ' [' . $songdata['difficulty'] . ']' . PHP_EOL;
 
-    $filtered_beatmap = array_filter($beatmap);
-    array_pop($filtered_beatmap);
-    $last_row_id = array_key_last($filtered_beatmap);
-
-    $image_height = ($size_row + $size_sub_beat * $songdata['difficulty_id']) * $last_row_id;
-
-    $size  = new Box($image_width, $image_height);
-    $image = $imagine->create($size);
+    $beatmap_last_row_id = array_key_last($beatmap);
 
     $used_vertical_offsets = [];
     $used_column_indexes = [];
+
     foreach ($beatmap as $row_id => $row) {
         foreach ($row as $boat_id => $beat) {
             $used_column_indexes[] = $beat['column_index'];
             $used_vertical_offsets[] = $beat['vertical_offset'];
         }
     }
+
     $min_column_index = min(...$used_column_indexes);
     $max_column_index = max(...$used_column_indexes);
     $min_vertical_offset = min(...$used_vertical_offsets);
     $max_vertical_offset = max(...$used_vertical_offsets);
 
-    foreach ($beatmap as $row_id => $row) {
-        if ($row_id > $last_row_id) {
+    $image_height = $size_row * $beatmap_last_row_id * $scale_vertical + $margin_top;
+    $image = $imagine->create(new Box($image_width, $image_height));
+
+    // Vars to draw sliders
+    $last_row_id = null;
+    $last_beat_type = null;
+    $last_x = null;
+    $last_y = null;
+
+    foreach (array_reverse($beatmap) as $row_id => $row) {
+        if ($row_id > $beatmap_last_row_id) {
             continue;
         }
-        // echo $row_id;
+
         foreach ($row as $boat_id => $beat) {
-            $beat_offset_height = $image_height - $row_id * $size_row * $scale_vertical + $size_sub_beat * $scale_subbeat * ($beat['vertical_offset'] ?: 1) * $scale_vertical / $max_vertical_offset;
-            $beat_offset_width = $beat_x_start
+            $beat_offset_y = $image_height - $row_id * $size_row * $scale_vertical + $size_sub_beat * $scale_subbeat * ($beat['vertical_offset'] + 1) * $scale_vertical / $max_vertical_offset;
+            $beat_offset_x = $beat_x_start
                 + $beat_x_width * $scale_horizontal * ($beat['column_index'] + 1) / $max_column_index;
 
-            // echo sprintf('%03.02fx%04.02f - Index %s', $beat_offset_width, $beat_offset_height, $beat['column_index']) . PHP_EOL;
+            if ($beat['beat_type'] !== 0 && $last_beat_type !== 0 && $last_x && $last_y) {
+                $image->draw()
+                    ->line(new Point($last_x, $last_y), new Point($beat_offset_x, $beat_offset_y), $image->palette()->color($color_slider_slide), $size_slider_line);
+            }
 
-            // echo ' ' . $boat_id . ' ' . $beat_offset_height;
             $color = $beat['beat_type'] === 0 ? $color_tap : $color_slider;
             $image->draw()
-                ->ellipse(new Point($beat_offset_width, $beat_offset_height), new Box($size_beat, $size_beat), $image->palette()->color($color), true);
+                ->ellipse(new Point($beat_offset_x, $beat_offset_y), new Box($size_beat, $size_beat), $image->palette()->color($color), true);
+
+            $last_x = $beat_offset_x;
+            $last_y = $beat_offset_y;
+            $last_beat_type = $beat['beat_type'];
         }
-        // echo PHP_EOL;
+        $last_row_id = $row_id;
+        $last_x = null;
+        $last_y = null;
+
     }
 
     $image->save(sprintf(
         '%s%s%s - %s.png',
         $path_beatmap_images,
         DS,
-        basename($songdata['song_filename'], '.json'),
+        basename($songdata['dalcom_beatmap_filename'], '.json'),
         $songdata['difficulty']
     ));
-
-    // echo '400x' . $image_height . PHP_EOL;
-
-// break;
 }
 
 echo 'Done.' . PHP_EOL;
